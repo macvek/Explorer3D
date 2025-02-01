b1766942412f070372067c081ff5c14e12e03c00
Maciej Aleksandrowicz <macvek@gmail.com>
2025-02-01 22:03:39 +0100

    Fixed issue by applying same transformations as OpenGL does

--- main.cxx
+++ main.cxx
@@ -41,6 +41,24 @@ struct M44 {
 		m[3][0] = 0;		  m[3][1] = 0;			m[3][2] = 0; m[3][3] = 1;
 	}
 
+	void FillFrom(M44& s) {
+		for (int y = 0; y < 4; ++y)
+			for (int x = 0; x < 4; ++x)
+				m[y][x] = s.m[y][x];
+	}
+
+	void Mult(M44& o) {
+		M44 r;
+		for (int l = 0; l < 4; ++l) {
+			r.m[l][0] = m[l][0] * o.m[0][0] + m[l][1] * o.m[1][0] + m[l][2] * o.m[2][0] + m[l][3] * o.m[3][0];
+			r.m[l][1] = m[l][0] * o.m[0][1] + m[l][1] * o.m[1][1] + m[l][2] * o.m[2][1] + m[l][3] * o.m[3][1];
+			r.m[l][2] = m[l][0] * o.m[0][2] + m[l][1] * o.m[1][2] + m[l][2] * o.m[2][2] + m[l][3] * o.m[3][2];
+			r.m[l][3] = m[l][0] * o.m[0][3] + m[l][1] * o.m[1][3] + m[l][2] * o.m[2][3] + m[l][3] * o.m[3][3];
+		}
+
+		FillFrom(r);
+	}
+
 	void Print() {
 		cout << " [ " << m[0][0] << "\t" << m[0][1] << "\t" << m[0][2] << "\t" << m[0][3] << " ] " << endl;
 		cout << " [ " << m[1][0] << "\t" << m[1][1] << "\t" << m[1][2] << "\t" << m[1][3] << " ] " << endl;
@@ -159,17 +177,20 @@ struct DrawPlane {
 	void applyMoves() {
 		if (moveAlongX != 0 || moveAlongZ != 0) {
 
-			Vec3F v = { moveAlongX, 0, moveAlongZ };
+			Vec3F v = { moveAlongX * moveSpeed, 0, moveAlongZ * moveSpeed };
 
-			M44 m; 
+			M44 mX; 
+			mX.asRotateX(rad(-aX));
+			M44 m;
 			m.asRotateY(rad(-aY));
-			Vec3F vY =  m.ApplyOnPoint(v);
-			m.asRotateX(rad(-aX));
-			Vec3F vX = m.ApplyOnPoint(vY);
+
+			m.Mult(mX);
+
+			Vec3F vRotated =  m.ApplyOnPoint(v);
 			
-			posX += moveSpeed * vX.x;
-			posY += moveSpeed * vX.y;
-			posZ += moveSpeed * vX.z;
+			posX += vRotated.x;
+			posY += vRotated.y;
+			posZ += vRotated.z;
 		}
 	}
 
