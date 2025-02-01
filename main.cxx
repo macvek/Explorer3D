ef78ca1da79b5a84499f38a0e92fdf09f825ac53
Maciej Aleksandrowicz <macvek@gmail.com>
2025-02-01 09:12:24 +0100

    Initial playground with rotating quad

--- main.cxx
+++ main.cxx
@@ -28,6 +28,9 @@ struct OpenGLProperties {
 	}
 };
 
+GLfloat angleY = 45;
+GLfloat angleX = 45;
+
 struct AppContext {
 	SDL_GLContext glcontext;
 	SDL_Window* window = NULL;
@@ -84,65 +87,85 @@ struct Drawable {
 };
 
 
-struct Draw_017 : public Drawable {
+struct DrawPlane : public Drawable {
 
+	const int fovDiff = 1;
+	const double fovMax = 160;
+	const double fovMin = 5;
+
+	const double nearPlane = 0.1;
+	const double farPlane = 50;
+
+	GLdouble FOV = 60;
+	int frames = 0;
+
+	bool refresh = false;
 	void init() {
-		std::cout << "\nDraw_017... \n\n";
-		
-		glClear(GL_COLOR_BUFFER_BIT);
 
-		glColor4f(1, 0, 1, 1);
 		glViewport(0, 0, WIDTH, HEIGHT);
+		refresh = true;
+	}
+
+	void updateFov(double newFov) {
+		FOV = max<double>(min<double>(newFov, fovMax), fovMin);
+		refresh = true;
+	}
+
+	void updateProjection() {
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		glOrtho(-1, 1, 1, -1, -1, 1);
+
+		bool perspetive = true;
+		if (perspetive) {
+			GLdouble aspectRatio = WIDTH / HEIGHT;
+			GLdouble tangent = tan(FOV / 2 * M_PI / 180);
+			GLdouble right = nearPlane * tangent;
+			GLdouble top = right / aspectRatio;
+
+			glFrustum(-right, right, -top, top, nearPlane, farPlane);
+		}
+		else {
+			glOrtho(-1, 1, -1, 1, nearPlane, farPlane);
+		}
 
 		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		glBegin(GL_QUADS);
-		glVertex2f(-0.6, 0.0);
-		glVertex2f(0.0, 0.6);
-		glVertex2f(0.6, 0.0);
-		glVertex2f(0.0, -0.6);
-		glEnd();
-		GLuint texName;
-		glGenTextures(1, &texName);
-		glBindTexture(GL_TEXTURE_2D, texName);
+	}
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-		glCopyTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 44, 44, 512, 512, 0);
+	void frame() {
+		++frames;
 
-		
+		if (refresh) {
+			refresh = false;
+			updateProjection();
+		}
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		glEnable(GL_TEXTURE_2D);
-		glTranslatef(0, -0.5,0 );
-		glRotatef(45, 1, 1, 0);
-		glBegin(GL_QUADS);
-		glTexCoord2f(0, 0);  glVertex2f(-0.4, -0.4);
-		glTexCoord2f(4, 0); glVertex2f(0.4, -0.4);
-		glTexCoord2f(4, 4); glVertex2f(0.4, 0.4);
-		glTexCoord2f(0, 4); glVertex2f(-0.4, 0.4);
-		glEnd();
 		glLoadIdentity();
-		glTranslatef(0, 0.5, 0);
-		glRotatef(60, -1, 0.5, 0);
-		glBegin(GL_QUADS);
-		glTexCoord2f(0, 0);  glVertex2f(-0.4, -0.4);
-		glTexCoord2f(4, 0); glVertex2f(0.4, -0.4);
-		glTexCoord2f(4, 4); glVertex2f(0.4, 0.4);
-		glTexCoord2f(0, 4); glVertex2f(-0.4, 0.4);
-		glEnd();
+
+		glPushMatrix();
+			glTranslatef(0, 0, -3);
+			drawQuad();
+		glPopMatrix();
 		
 		SDL_GL_SwapWindow(App.window);
 	}
 
-	void frame() {
+	void drawQuad() {
+		glPushMatrix();
+		GLdouble spin = 0.2 * frames;
+		glRotatef(spin, 1, 0.4, 0.2);
 
+		glColor4f(1, 1, 1, 1);
+		glBegin(GL_QUADS);
+
+		glColor3f(1, 1, 0);  glVertex2f(-1, -1);
+		glColor3f(1, 0, 1);  glVertex2f(1, -1);
+		glColor3f(0, 1, 1);  glVertex2f(1, 1);
+		glColor3f(1, 1, 1);  glVertex2f(-1, 1);
+
+		glEnd();
+		glPopMatrix();
 	}
+
 };
 
 int main(int argc, char** argv) {
@@ -153,9 +176,10 @@ int main(int argc, char** argv) {
 	}
 
 	App.openglProperties.toStream(cout);
+	SDL_SetWindowRelativeMouseMode(App.window, true);
 
 	bool showEvent = false;
-	Drawable& d = Draw_017();
+	DrawPlane d;
 
 	d.init();
 	
@@ -163,9 +187,28 @@ int main(int argc, char** argv) {
 	int milis = 1000 / 60;
 
 	
+
 	for (;;) {
 		SDL_Event event;
 		if (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EVENT_MOUSE_MOTION) {
+				SDL_MouseMotionEvent* mouseEvent = (SDL_MouseMotionEvent*) &event;
+				angleX += mouseEvent->xrel;
+				angleY += mouseEvent->yrel;
+			}
+			
+			if (event.type == SDL_EVENT_KEY_UP) {
+				SDL_KeyboardEvent* keyEvent = (SDL_KeyboardEvent*)&event;
+				if (keyEvent->key == SDLK_KP_PLUS) {
+					d.updateFov(d.FOV + d.fovDiff);
+					cout << "FOV:" << d.FOV << endl;
+				}
+				else if (keyEvent->key == SDLK_KP_MINUS) {
+					d.updateFov(d.FOV - d.fovDiff);
+					cout << "FOV:" << d.FOV << endl;
+				}
+			}
+
 			if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) {
 				break;
 			}
@@ -176,6 +219,7 @@ int main(int argc, char** argv) {
 		else {
 			SDL_Delay(milis);
 			d.frame();
+			
 		}
 	}
 
